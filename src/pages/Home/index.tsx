de77585fdff5e04be3f61a3c6a1270b79090b3e0
Pablo Borges Martins <pablo.martins1998@gmail.com>
2022-07-25 17:39:04 -0300

    refactor: Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,11 @@
 import {
-    Container
+  Container
 } from './styles';
 
 export function Home() {
-    return (
-        <Container>
-            <h1>Hello World!</h1>
-        </Container>
-    );
+  return (
+    <Container>
+      <h1>Hello World!</h1>
+    </Container>
+  );
 }
